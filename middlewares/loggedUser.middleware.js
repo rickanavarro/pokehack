35aa49eac88d3158f36a7edbffb2e0c77e96043e
JLMarse <martinezserrano.joseluis@gmail.com>
2023-05-19 09:28:58 +0200

    last deplyed

--- middlewares/loggedUser.middleware.js
+++
@@ -1,7 +0,0 @@
-// const updateLoggedUser = (req, res, next) => {
-//     res.locals.loggedUser = req.session.currentUser
-//     res.locals.userisAdmin = req.session.currentUser?.role === 'ADMIN'
-//     next()
-// }
-
-// module.exports = { updateLoggedUser }
